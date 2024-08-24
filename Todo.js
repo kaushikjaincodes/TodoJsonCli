cba5d9e6dee819eec1b01ac96de15e97227c367d
Kaushik Jain <115381741+kaushikjaincodes@users.noreply.github.com>
2024-08-25 00:27:36 +0530

    Initial
    added 2 files

---
+++ Todo.js
@@ -0,0 +1,85 @@
+const fs = require('fs');
+const { Command } = require('commander');
+const path = require('path');
+const program = new Command();
+
+program
+    .name("Todo json")
+    .description("Commands to manage todo using CLI")
+    .version("1.0.0");
+
+program.command("add")
+    .description("Add a todo")
+    .argument("<Time>", "Enter the finish time")
+    .argument("<Todo>", "Enter the todo")
+    .action((Time, Todo) => {
+        const obj = {
+            Deadline: Time,
+            Title: Todo
+        };
+
+        const today = new Date();
+        let date = today.toISOString().slice(0, 10);
+        const fileName = `${date}.json`;
+        const filePath = path.join(__dirname, fileName);
+
+        fs.readFile(filePath, 'utf-8', (err, data) => {
+            let jsonData = [];
+            if (err) {
+                if (err.code === 'ENOENT') {
+                    jsonData = [obj];
+                } else {
+                    console.log(err);
+                    return;
+                }
+            } else {
+                if (data) {
+                    jsonData = JSON.parse(data); 
+                }
+                jsonData.push(obj); 
+            }
+
+            fs.writeFile(filePath, JSON.stringify(jsonData, null, 2), 'utf-8', (err) => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log("Success");
+                }
+            });
+        });
+    });
+
+program.command("remove")
+    .description("To Remove a Todo")
+    .argument('<Todo>','The todo to delete')
+    .argument('<Date>','Enter the date in the format yyyy-mm-dd')
+    .action((Todo,Date)=>{
+        const fileName = `${Date}.json`;
+        const filePath = path.join(__dirname, fileName);
+        fs.readFile(filePath,'utf-8',(err,data)=>{
+            if(err){
+                if (err.code === 'ENOENT') {
+                    console.log("File Does not exist");
+                    return;
+                } else {
+                    console.log(err);
+                    return;
+                }
+            }else{
+                let jsonData = JSON.parse(data);
+                jsonData = jsonData.filter(obj => obj.Title !== Todo);
+
+                const updated = JSON.stringify(jsonData,null,2);
+
+                fs.writeFile(filePath,updated,'utf-8',(err)=>{
+                    if(err){
+                        console.log(err);
+                    }else{
+                        console.log("Task Deleted");
+                    }
+                })
+            }
+        })
+    })
+    
+program.parse();
